a7d1abe5037200e9c92d8c01c381fafd08e72f13
Gabi Beyer <gabib@live.com>
2019-03-05 16:10:56 -0800

    Impliment cli to be consistent with kata
    Use the urfave/cli golang library for lvm-snapshotter
    library. It takes three flags: a unix address, a vgname
    and a lvpoolname.
    
    Signed-off-by: Gabi Beyer <gabib@live.com>

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/urfave/cli"
 	"google.golang.org/grpc"
 
 	snapshotsapi "github.com/containerd/containerd/api/services/snapshots/v1"
@@ -14,15 +15,16 @@ import (
 	lvms "github.com/ganeshmaharaj/lvm-snapshotter/lvmsnapshotter"
 )
 
-func main() {
-	// Provide a unix address to listen to, this will be the `address`
-	// in the `proxy_plugin` configuration.
-	// The root will be used to store the snapshots.
-	if len(os.Args) < 4 {
-		fmt.Printf("invalid args: usage: %s <unix addr> <vgname> <lvpoolname>\n", os.Args[0])
-		os.Exit(1)
-	}
+var usage = fmt.Sprint(`an image layering tool using the containerd shapshots
+	 API and the thin logical volume device mapper of LVM.
 
+	 To run lvm-snapshotter a path to the unix socket address, volume group name,
+	 and logical volume pool name are required:
+
+	 $ lvm-snapshotter --addr /path/to/socket --vgname volumegroup --lvpoolname poolname
+`)
+
+func prepareSnapshotter(addr, vgname, lvpoolname string) error {
 	// Create a gRPC server
 	rpc := grpc.NewServer()
 
@@ -30,10 +32,9 @@ func main() {
 	// snapshotter and a root directory. Your custom snapshotter will be
 	// much more useful than using a snapshotter which is already included.
 	// https://godoc.org/github.com/containerd/containerd/snapshots#Snapshotter
-	sn, err := lvms.NewSnapshotter(os.Args[2], os.Args[3])
+	sn, err := lvms.NewSnapshotter(vgname, lvpoolname)
 	if err != nil {
-		fmt.Printf("error: %v\n", err)
-		os.Exit(1)
+		return err
 	}
 	defer sn.Close()
 
@@ -55,13 +56,53 @@ func main() {
 	}()
 
 	// Listen and serve
-	l, err := net.Listen("unix", os.Args[1])
+	l, err := net.Listen("unix", addr)
 	if err != nil {
-		fmt.Printf("error: %v\n", err)
-		os.Exit(1)
+		return err
 	}
 	if err := rpc.Serve(l); err != nil {
-		fmt.Printf("error: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func createApp() error {
+	var addr string
+	var vgname string
+	var lvpoolname string
+
+	app := cli.NewApp()
+	app.Name = "lvmsnapshotter"
+	app.Usage = usage
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "addr",
+			Usage: "the socket the snapshotter will listen on, it is the 'address' in the containerd 'proxy plugin' configuration.",
+			Destination: &addr,
+		},
+		cli.StringFlag{
+			Name:  "vgname",
+			Usage: "name of created volume group",
+			Destination: &vgname,
+		},
+		cli.StringFlag{
+			Name:  "lvpoolname",
+			Usage: "name of logical volume pool",
+			Destination: &lvpoolname,
+		},
+	}
+	app.Action = func(ctx *cli.Context) error {
+		if ctx.NumFlags() != 3 {
+			return fmt.Errorf("incorrect usage, view help for correct argument usage")
+		}
+		return prepareSnapshotter(addr, vgname, lvpoolname)
+	}
+	return app.Run(os.Args)
+}
+
+func main() {
+	if err := createApp(); err != nil {
+		fmt.Println("error:", err)
 		os.Exit(1)
 	}
 }
